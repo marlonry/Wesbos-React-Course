bb3aee5dfca6196a2448eeb13a6254b9f88aee5b
unknown <marlonspr@hotmail.com>
2019-07-31 13:34:44 +1200

    component

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -4,13 +4,28 @@ import Order from './Order';
 import Header from './Header';
  
 export default class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish => {
+        // 1. take a copy of existing state // never mutate the state
+        const fishes = { ...this.state.fishes }; // spread
+        //const fishes = Object.assign({}, this.state.fishes); oobject.assign
+
+        // 2. add out new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish; 
+
+        // 3. ser the new fishes object to state
+        this.setState({ fishes });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                 </div> 
-                <Inventory/>
+                <Inventory addFish={this.addFish}/>
                 <Order />
             </div>
         )
--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 export default class StorePicker extends React.Component {
     constructor(props) {
-        super(props);   
-        this.state = {
-            message: 'Hello there',
-        }
+        super(props);
+        this.myInput = React.createRef();
+        // this.state = {
+        //     message: 'Hello there',
+        // }
+
+        // this.goToStore = this.goToStore.bind(this); // binding methods when using this
+    }
+
+    //myInput = React.createRef(); // this can be done as property with the funky syntax or can be set on the constructor as an attribute
+
+    goToStore = (e) => {
+        //1. stop form subsmission
+        e.preventDefault();
+        //2. get the text from that input
+        const storeName = (this.myInput.current.value);
+        //3. change the page to /store/whateever they entered
+        this.props.history.push(`/store/${storeName}`); // this history.push() method can only be accessed because store picker inhetits all the methods of its parents via props in this case the parent is the router
     }
 
     render() {
         // return React.createElement('p', { id: 'paragraph', className: 'paragraph'}, 'Hello there' );
         // return only one element out a render or use react.fragment or <> </> empty tags for adjacent elements
-        return (
-            <form action="" className="store-selector">
-                <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Enter a store name"/>
-                <button type="submit">Visit Store -></button>
+        return ( 
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2> Please enter a store </h2>
+                <input 
+                    type= "text" 
+                    ref={this.myInput} 
+                    required 
+                    placeholder="Enter a store name" 
+                    defaultValue={getFunName()}/>
+                <button type="submit"> Visit Store -> </button> 
             </form>
         );
     }
